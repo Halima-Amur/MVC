aff4e38adc25c4019e32b00eedff4247f0f9e770
Halima-Amur <halim9amer@gmail.com>
2024-01-21 00:15:22 +0400

    MVC

---
+++ MVC/Controllers/ToDoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+
+namespace MVC.Controllers
+{
+	public class ToDoController : Controller 
+	{
+		private readonly ApplicationDbContext _context;
+        public ToDoController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var todo = _context.ToDos.ToList();
+
+			return View(todo);
+		}
+	}
+}
+
---
+++ MVC/Models/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+
+		public class ToDo
+		{
+			public int Id { get; set; }
+
+        [Required(ErrorMessage ="ToDo Name is required")]
+
+        public  string name { get; set; }
+
+            public bool isFinished { get; set; }
+
+            public string? descriotion { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
+
+
